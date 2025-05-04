4817a71a61d205dec488648d31bed6ab981857a6
Jayant Kumar <123481470+jayantkumar1604@users.noreply.github.com>
2025-05-04 20:37:37 +0530

    git

---
+++ Microservice 1 currency-exchange-sample-service/src/main/java/com/gfg/microservices/currencyexchangesampleservice/CurrencyExchangeServiceSampleApplication.java
@@ -0,0 +1,13 @@
+package com.gfg.microservices.currencyexchangesampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceSampleApplication.class, args);
+	}
+
+}
